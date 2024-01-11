d6e03a36d238afe259b2b536d766fa281f4d2965
Marakuya90 <zhilzhil@mail.ru>
2024-01-11 11:24:14 +0300

    sidebar

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+console.log('loaded')
+</script>
+
+<template>
+  <div>
+    <section class="grid" style="min-width: 100vh">
+      <aside>Sidebar</aside>
+      <div>
+        <slot />
+      </div>
+    </section>
+  </div>
+
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+
+</style>
