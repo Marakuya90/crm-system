500c6e7706a2e18f9971fddd1b1bbd7aad0e4744
Yulya Schegoleva <zhilzhil@mail.ru>
2024-01-12 12:16:42 +0300

    invite appwrite lib, db, storage

---
+++ app.constants.ts
@@ -0,0 +1,7 @@
+export const APP_WRITE_ID = '65a017cb947a3a2b8206';
+export const DB_ID = 'crm-base';
+export const COLLECTIONS_DEALS = 'deals';
+export const COLLECTIONS_CUSTOMERS = 'customers';
+export const COLLECTIONS_COMMENTS = 'comments';
+export const STORAGE_ID = 'storage';
+
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+useHead({
+  title: 'Login | CRM System'
+})
+
+const emailRef = ref('')
+
+watch(emailRef, () => {
+  console.log(emailRef)
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+      <form>
+        <UiInput
+            placeholder="Email"
+            type="email"
+            v-model="emailRef"
+            class="mb-3"/>
+      </form>
+    </div>
+
+  </div>
+</template>
